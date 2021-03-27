d0e122545bd22ffeac975726a98cec5a15f9ac14
SamarpreetSingh787 <78362149+SamarpreetSingh787@users.noreply.github.com>
2021-03-27 06:45:49 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+function take_snapshot() {
+    Webcam.snap(function(uri){
+        document.getElementById("result").innerHTML = '<img id="capt_img" src="'+uri+'">';
+    });
+}
+
+console.log("ml5 version: " , ml5.version );
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/SmHve7FNZ/model.json" , modelLoaded());
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+}
