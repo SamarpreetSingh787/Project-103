04b8d90eea3d0d2c8491c36ea8e9592fc73bb07e
SamarpreetSingh787 <78362149+SamarpreetSingh787@users.noreply.github.com>
2021-03-30 03:23:25 -0700

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function take_snapshot() {
 
 console.log("ml5 version: " , ml5.version );
 
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/SmHve7FNZ/model.json" , modelLoaded());
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/SmHve7FNZ/model.json" , modelLoaded);
 
 function modelLoaded() {
     console.log("Model Loaded!")
